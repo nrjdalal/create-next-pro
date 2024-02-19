14467019dac12c87e46e6c108c7f9972c9025516
Neeraj Dalal <admin@nrjdalal.com>
2024-02-19 22:58:29 +0530

    v0: init template

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import ora from "ora"
 import createNextAppOptions from "./utils/createNextApp"
 import getPackageManagers from "./utils/packageManagers"
 
+// 0. initializing variables
 let opts
-let spinner = ora()
+const spinner = ora()
 
 // 1. select a package manager
 const { manager, runner } = await select({
@@ -17,10 +18,6 @@ const { manager, runner } = await select({
 // 2. create a Next.js app
 opts = await createNextAppOptions()
 spinner.start("Creating a Next.js app")
-try {
-  spinner.succeed(chalk.green("Successfully created a Next.js app"))
-  await $`rm -rf test`
-  await $`${runner} create-next-app test --ts --eslint --tailwind ${opts.noSrcDir} --app --import-alias "@/*"`
-} catch {
-  spinner.fail(chalk.red("Failed to create a Next.js app"))
-}
+await $`rm -rf test`
+await $`${runner} create-next-app test --ts --eslint --tailwind --app ${opts.noSrcDir} --import-alias "@/*"`
+spinner.succeed(chalk.green("Successfully created a Next.js app"))
