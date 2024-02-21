6f30dffadfb70ce7cbd82e8d8e177000fed6bfd1
Neeraj Dalal <admin@nrjdalal.com>
2024-02-21 13:28:00 +0530

    added more steps

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,29 @@ import ora from "ora"
 import createNextAppOptions from "./utils/createNextApp.js"
 import getPackageManagers from "./utils/packageManagers.js"
 
-// 0. initializing variables
-let cmd: any, draft: any, opts: any
+// ~ initializing variables
+let draft: any, opts: any, src: string
 const spinner = ora()
 
-// 1. select a package manager
+// ~ select a package manager
 const { manager, runner }: any = await select({
   message: "Select a package manager",
   choices: await getPackageManagers(),
 })
 
-// 2. create a Next.js app
+// ~ create a Next.js app
 opts = await createNextAppOptions()
+opts.noSrcDir !== "--no-src-dir" ? (src = "src/") : (src = "")
 spinner.start("Creating a Next.js app")
 await $`${runner} create-next-app . --ts --eslint --tailwind --app ${opts.noSrcDir} --import-alias @/*`
-spinner.succeed(chalk.green("Successfully created a Next.js app"))
+spinner.succeed(chalk.green("Successfully created app"))
 
-// 3. install prettier and plugins
-spinner.start("Installing prettier and plugins")
+// ~ add prettier to the app
+spinner.start("Adding prettier")
 await $`${manager} add -D prettier @ianvs/prettier-plugin-sort-imports prettier-plugin-tailwindcss`
-spinner.succeed(chalk.green("Successfully installed prettier and plugins"))
+spinner.succeed(chalk.green("Successfully added prettier"))
 
-// 4. configure prettier
+// ~ configure prettier
 spinner.start("Configuring prettier")
 draft = `/** @type {import("prettier").Config} */
 module.exports = {
@@ -42,7 +43,55 @@ module.exports = {
 await fs.writeFile("prettier.config.js", draft, "utf-8")
 spinner.succeed(chalk.green("Successfully configured prettier"))
 
-// 5. add shadcn/ui to the app
-spinner.start("Adding shadcn/ui to the app")
+// ~ add shadcn-ui to the app
+spinner.start("Adding shadcn-ui to the app")
 await $`${runner} shadcn-ui init -d`
-spinner.succeed(chalk.green("Successfully added shadcn/ui to the app"))
+spinner.succeed(chalk.green("Successfully added shadcn-ui"))
+
+// ~ add next-themes to the app
+spinner.start("Adding next-themes to the app")
+await $`${manager} add next-themes`
+spinner.succeed(chalk.green("Successfully added next-themes"))
+
+// ~ configure next-themes
+spinner.start("Configuring next-themes")
+draft = `"use client"
+
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+`
+await fs.mkdir(src + "components/ui", { recursive: true })
+await fs.writeFile(src + "components/ui/theme-provider.tsx", draft, "utf-8")
+
+// ~ add drizzle to the app
+spinner.start("Adding drizzle to the app")
+await $`${manager} add drizzle-orm postgres`
+await $`${manager} add -D drizzle-kit`
+spinner.succeed(chalk.green("Successfully added drizzle"))
+
+// ~ configure drizzle
+spinner.start("Configuring drizzle")
+draft = `import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+
+const queryClient = postgres("postgres://postgres:adminadmin@0.0.0.0:5432/db")
+export const db = drizzle(queryClient)
+`
+await fs.mkdir(src + "lib/db/drizzle", { recursive: true })
+await fs.writeFile(src + "lib/db/drizzle/index.ts", draft, "utf-8")
+spinner.succeed(chalk.green("Successfully configured drizzle"))
+
+// ~ cleanup
+spinner.start("Cleaning up")
+try {
+  await fs.rm(".git", { recursive: true, force: true })
+  await $`git init`
+  await $`git add .`
+  await $`git commit -m "Init via ${runner} create-next-pro"`
+} catch {}
+spinner.succeed(chalk.green("Successfully cleaned up"))
